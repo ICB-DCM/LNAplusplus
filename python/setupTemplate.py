206be47ad573606e80f37280a638bd0242db7265
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-04-18 10:42:45 +0200

    Update blitz++ to 1.0.1 to work with recent g++ versions. Set default include and library locations. Update doc. Closes #5.

--- python/setupTemplate.py
+++ python/setupTemplate.py
@@ -14,9 +14,10 @@ numpyPath = get_include()
 module1 = Extension('myModuleLNA',
                     define_macros = [('MAJOR_VERSION', '1'),
                                      ('MINOR_VERSION', '0')],
-                    include_dirs = ['/usr/local/include', 'myModule/C', '../include', '../src', '../libraries/blitz-0.10', '../libraries/cvodes-2.7.0/install/include', numpyPath]+INCLUDE_DIRS,
+                    include_dirs = ['/usr/local/include', 'myModule/C', '../include', '../src', 
+								'../libraries/blitz-1.0.1/install/include', '../libraries/cvodes-2.7.0/install/include', numpyPath]+INCLUDE_DIRS,
                     libraries = ['stdc++', 'sundials_cvodes', 'blitz', 'sundials_nvecserial'], #,'python3.4'],
-                    library_dirs = ['/usr/local/lib']+LIB_DIRS,
+                    library_dirs = ['/usr/local/lib', '../libraries/blitz-1.0.1/install/lib', '../libraries/cvodes-2.7.0/install/lib/']+LIB_DIRS,
                     sources = ['../src/computeLinearNoise.cpp', 'myModule/myModule_LNA.cpp']\
 						+ glob('myModule/C/*.c'), 
 					runtime_library_dirs=['/usr/local/lib'],
