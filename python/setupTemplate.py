a4c810fbb3e1978954e7fafc4d5d6f1cba9c8118
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-04-20 09:45:31 +0200

    User-provided include / library directories should have higher precedence than default values. Fixes #9

--- python/setupTemplate.py
+++ python/setupTemplate.py
@@ -21,10 +21,10 @@ for key, value in cfg_vars.items():
 module1 = Extension('myModuleLNA',
                     define_macros = [('MAJOR_VERSION', '1'),
                                      ('MINOR_VERSION', '0')],
-                    include_dirs = ['/usr/local/include', 'myModule/C', '../include', '../src', 
-								'../libraries/blitz-1.0.1/install/include', '../libraries/cvodes-2.7.0/install/include', numpyPath]+INCLUDE_DIRS,
+                    include_dirs = INCLUDE_DIRS + ['/usr/local/include', 'myModule/C', '../include', '../src', 
+								'../libraries/blitz-1.0.1/install/include', '../libraries/cvodes-2.7.0/install/include', numpyPath],
                     libraries = ['stdc++', 'sundials_cvodes', 'blitz', 'sundials_nvecserial'], #,'python3.4'],
-                    library_dirs = ['/usr/local/lib', '../libraries/blitz-1.0.1/install/lib', '../libraries/cvodes-2.7.0/install/lib/']+LIB_DIRS,
+                    library_dirs = LIB_DIRS + ['/usr/local/lib', '../libraries/blitz-1.0.1/install/lib', '../libraries/cvodes-2.7.0/install/lib/'],
                     sources = ['../src/computeLinearNoise.cpp', 'myModule/myModule_LNA.cpp']\
 						+ glob('myModule/C/*.c'), 
 					runtime_library_dirs=['/usr/local/lib'],
