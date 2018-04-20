680db3451bd7662d18586c9007df46d4a3e36e16
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-04-20 10:17:12 +0200

    osx CI build fails because of case-insensitive file system? test different installation path

--- models/runCI.sh
+++ models/runCI.sh
@@ -9,14 +9,14 @@ set -e
 cd $BASE_DIR/libraries
 tar -xzf blitz-1.0.1.tar.gz
 cd blitz-1.0.1
-./configure --with-pic --prefix=`pwd`/install
+./configure --with-pic --prefix=BASE_DIR/libraries/install/blitz-1.0.1
 make && make install
 
 ## cvodes
 cd $BASE_DIR/libraries
 tar -xzf cvodes-2.7.0.tar.gz
 cd cvodes-2.7.0
-./configure --with-pic  --prefix=`pwd`/install
+./configure --with-pic --prefix=BASE_DIR/libraries/install/cvodes-2.7.0
 make && make install
 
 # run example
