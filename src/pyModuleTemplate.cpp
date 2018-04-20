1412919c847f726cbf641ddc234db10cb040136b
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-04-20 15:26:37 +0200

    Document python interface. #10, #14

--- src/pyModuleTemplate.cpp
+++ src/pyModuleTemplate.cpp
@@ -4,7 +4,28 @@
 // Methods table
 static PyMethodDef myModuleMethods[] = {
     {"LNA",  (PyCFunction)LNA_LNA,  METH_VARARGS | METH_KEYWORDS,
-     "Compute the linear noise approximation."},
+     "LNA(Theta, time, Y0 = None, V0 = None, merr = None, obsVar = None, computeSens = False, computeSens2 = False)"
+     " -> MRE, Var [, dMRE, dSigma, [d2MRE, d2Sigma]]\n\n"
+     "Compute the linear noise approximation.\n\n"
+     "Parameters:\n"
+     "-----------\n"
+     "Theta (numeric list): model parameters\n"
+     "time (numeric list): output times\n"
+     "Y0 (numeric list): initial values for each species\n"
+     "V0 (numeric list): initial values for each entry of the (upper triangular portion of the) covariance matrix in column-major ordering\n"
+     "merr (numeric or numeric list): measurement error, single value for all observables or list with one value per observable\n"
+     "obsVar (numeric list): list of observable state indices (1-based!!)\n"
+     "computeSens (boolean): compute first-order sensitivities\n"
+     "computeSens2 (boolean): compute second-order sensitivities\n"
+     "\n"
+     "Output:\n"
+     "-------\n"
+     "MRE: solution of the macroscopic rate equation for the observed variables specified\n"
+     "Var: block cross-covariance matrix for the observed variables specified\n"
+     "dMREdTheta: sensitivity tensor of the MRE for each observed variable with respect to each parameter, at each time point\n"
+     "dVardTheta: sensitivity tensor of the block cross-covariance for each entry of the covariance matric w.r.t. all parameters\n"
+     "d2MREdTheta2: second order sensitivity tensor of the MRE solution\n"
+     "d2VardTheta2: second order sensitivity tensor of the block cross-covariance solution\n"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
@@ -12,7 +33,7 @@ static PyMethodDef myModuleMethods[] = {
 static struct PyModuleDef myModule = {
    PyModuleDef_HEAD_INIT,
    "myModule",   /* name of module */
-   NULL, /* module documentation, may be NULL */
+   "myModule  model created using LNA++", /* module documentation, may be NULL */
    -1,       /* size of per-interpreter state of the module,
                 or -1 if the module keeps state in global variables. */
    myModuleMethods
