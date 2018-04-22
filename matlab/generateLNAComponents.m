91623c8a6c9b89d571f3414c529e474198ca408a
JanHasenauer <jan.hasenauer@gmx.de>
2018-04-22 20:35:03 +0200

    extension of generateLNAComponents
    to avoid unnecessary factions in the right-hand side of the vector field

--- matlab/generateLNAComponents.m
+++ matlab/generateLNAComponents.m
@@ -61,9 +61,11 @@ t = sym('t', 'real');
 
 %% add initial conditions to the vector of model parameters
 phi0 = sym('phi0', [1, length(phi)]);
-assume(phi0,'real')
-
 Theta = [Theta, phi0]; % necessary for sensitivities wrt initial conditions
+
+assume(Theta>0)
+assume(phi>0)
+assume(phi0>0)
 
 reactionFlux = reactionFluxFun(phi, t, Theta);
 
@@ -86,13 +88,13 @@ d2AdTheta2  = Jacobian(dAdTheta, Theta);
 d2AdPhi2    = Jacobian(dAdPhi, phi);
 
 % E
-Efunc           = S*sqrt(diag(reactionFlux));
+Efunc       = S*sqrt(diag(reactionFlux));
 
 % sensitivities of E
-dEdTheta    = Jacobian(Efunc, Theta);
-d2EdTheta2   = Jacobian(dEdTheta, Theta);
-dEdPhi      = Jacobian(Efunc, phi);
-d2EdPhi2    = Jacobian(dEdPhi, phi);
+dEdTheta    = simplifyExpr(Jacobian(Efunc, Theta));
+d2EdTheta2  = simplifyExpr(Jacobian(dEdTheta, Theta));
+dEdPhi      = simplifyExpr(Jacobian(Efunc, phi));
+d2EdPhi2    = simplifyExpr(Jacobian(dEdPhi, phi));
 
 %% solve for the initial steady state 
 nvar = length(phi);
@@ -106,8 +108,6 @@ if COMPUTE_Y0
 else
     Y0 = zeros(1,nvar); % placeholder
 end
-% [V0 systemJacobian MI] = solveSS_var(A,E,F,S,phi,Theta,sym(Y0));
-% [V0 systemJacobian] = solveSS_var(A,E,F,S,phi,Theta,Y0);
 
 if COMPUTE_V0
     V0 = solveSS_var(Afunc,Efunc,reactionFlux,S,phi,Theta,phi0);
@@ -134,23 +134,17 @@ sysVar = [sysVar reshape(Phi,1,[])];
 dVdt = Afunc*V + V*Afunc.' + subs(Efunc*Efunc.');
 RHS = [S*reactionFlux; dVdt(find(triu(ones(size(dVdt))))); reshape(Afunc*Phi,[],1)];
 
-systemJacobian = Jacobian(RHS, sysVar);
+systemJacobian = simplifyExpr(Jacobian(RHS, sysVar));
 systemJacobian_diag = diag(systemJacobian); % just the diagonal
-
-% preconditioner
-% gamma=sym('gamma','real');
-% M=eye(size(systemJacobian))-gamma*systemJacobian;
-
-% MI = pinv(M);
 
 %% initial sensitivities
 disp('computing initial sensitivities')
 
 if COMPUTE_Y0
     % dY/dTheta
-    S0  = Jacobian(Y0, Theta);
+    S0  = simplifyExpr(Jacobian(Y0, Theta));
     % d2Y/dTheta2
-    S20 = Jacobian(S0, Theta);
+    S20 = simplifyExpr(Jacobian(S0, Theta));
 else
    S0   = zeros(1,nvar*npar); % placeholder
    S20  = zeros(1,nvar*npar*npar); % placeholder
@@ -158,136 +152,32 @@ end
 
 if COMPUTE_V0
     % dV/dTheta
-    SV0 = Jacobian(V0, Theta);
+    SV0 = simplifyExpr(Jacobian(V0, Theta));
     % d2V/dTheta2
-    S2V0 = Jacobian(SV0, Theta);
+    S2V0 = simplifyExpr(Jacobian(SV0, Theta));
 else
-    SV0     = zeros(1,nvar*(nvar+1)/2*npar); % placeholder
-    S2V0    = zeros(1,nvar*(nvar+1)/2*npar*npar); % placeholder
+    SV0  = zeros(1,nvar*(nvar+1)/2*npar); % placeholder
+    S2V0 = zeros(1,nvar*(nvar+1)/2*npar*npar); % placeholder
 end
 %% mixed second derivatives
 disp('Computing Mixed Second Derivatives')
-d2AdPhidTheta = Jacobian(dAdPhi, Theta);
-d2AdThetadPhi = Jacobian(dAdTheta, phi);
+d2AdPhidTheta = simplifyExpr(Jacobian(dAdPhi, Theta));
+d2AdThetadPhi = simplifyExpr(Jacobian(dAdTheta, phi));
 
-d2EdPhidTheta = Jacobian(dEdPhi, Theta);
-d2EdThetadPhi = Jacobian(dEdTheta, phi);
-
-%% generate the matlab functions based on these symbolic functions
-% disp('Generating m-files')
-% clear t, t=sym('t','real');
-% matlabFunction(reactionFlux, 'file', [dirName '/matlab/reactionFlux'], 'vars', {phi, t, Theta});
-% matlabFunction(J, 'file', [dirName '/matlab/J'], 'vars', {phi, t, Theta});
-% matlabFunction(dFdTheta, 'file', [dirName '/matlab/dFdTheta'], 'vars', {phi, t, Theta});
-% matlabFunction(d2fdTheta2, 'file', [dirName '/matlab/d2fdTheta2'], 'vars', {phi, t, Theta});
-% matlabFunction(Afunc, 'file', [dirName '/matlab/Afunc'], 'vars', {phi, t, Theta});
-% matlabFunction(dAdTheta, 'file', [dirName '/matlab/dAdTheta'], 'vars', {phi, t, Theta});
-% matlabFunction(dAdPhi, 'file', [dirName '/matlab/dAdPhi'], 'vars', {phi, t, Theta});
-% matlabFunction(d2AdTheta2, 'file', [dirName '/matlab/d2AdTheta2'], 'vars', {phi, t, Theta});
-% matlabFunction(d2AdPhi2, 'file', [dirName '/matlab/d2AdPhi2'], 'vars', {phi, t, Theta});
-% matlabFunction(Efunc, 'file', [dirName '/matlab/Efunc'], 'vars', {phi, t, Theta});
-% matlabFunction(dEdTheta, 'file', [dirName '/matlab/dEdTheta'], 'vars', {phi, t, Theta});
-% matlabFunction(d2EdTheta2, 'file', [dirName '/matlab/d2EdTheta2'], 'vars', {phi, t, Theta});
-% matlabFunction(dEdPhi, 'file', [dirName '/matlab/dEdPhi'], 'vars', {phi, t, Theta});
-% matlabFunction(d2EdPhi2, 'file', [dirName '/matlab/d2EdPhi2'], 'vars', {phi, t, Theta});
-% matlabFunction(systemJacobian, 'file', [dirName '/matlab/systemJacobian'], 'vars', {phi, t, Theta});
-% matlabFunction(systemJacobian_diag, 'file', [dirName '/matlab/systemJacobian_diag'], 'vars', {phi, t, Theta});
-% 
-% % gamma=sym('gamma','real');
-% % matlabFunction(MI, 'file', [dirName '/matlab/MI'], 'vars', {phi, t, Theta, gamma});
-% 
-% matlabFunction(sym(Y0), 'file', [dirName '/matlab/Y0'], 'vars', {Theta});
-% matlabFunction(sym(V0), 'file', [dirName '/matlab/V0'], 'vars', {Theta});
-% 
-% matlabFunction(sym(S0), 'file', [dirName '/matlab/S0'], 'vars', {Theta});
-% matlabFunction(sym(SV0), 'file', [dirName '/matlab/SV0'], 'vars', {Theta});
-% matlabFunction(sym(S20), 'file', [dirName '/matlab/S20'], 'vars', {Theta});
-% matlabFunction(sym(S2V0), 'file', [dirName '/matlab/S2V0'], 'vars', {Theta});
-% 
-% matlabFunction(d2EdPhidTheta, 'file', [dirName '/matlab/d2EdPhidTheta'], 'vars', {phi, t, Theta});
-% matlabFunction(d2EdThetadPhi, 'file', [dirName '/matlab/d2EdThetadPhi'], 'vars', {phi, t, Theta});
-% 
-% matlabFunction(d2AdPhidTheta, 'file', [dirName '/matlab/d2AdPhidTheta'], 'vars', {phi, t, Theta});
-% matlabFunction(d2AdThetadPhi, 'file', [dirName '/matlab/d2AdThetadPhi'], 'vars', {phi, t, Theta});
+d2EdPhidTheta = simplifyExpr(Jacobian(dEdPhi, Theta));
+d2EdThetadPhi = simplifyExpr(Jacobian(dEdTheta, phi));
 
 %% generate the C library
 disp('Generating C source')
-% add the codegen tags to all the generated m files
-% codegenify( [dirName '/matlab'] );
 
-% % TODO: use the codegen configuration object to dynamically specify
-% % dimensions of input arguments phi and theta
+%old path
 olddir = cd([dirName '/matlab']);
-
-%     ' d2EdTheta -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-
-% codegen_cmd = sprintf(['codegen -v -O disable:openmp -c -config:lib -d %s '...
-%     ' reactionFlux -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' J -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' dFdTheta -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' d2fdTheta2 -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' Afunc -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' dAdTheta -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' dAdPhi -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' d2AdPhi2 -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...    
-%     ' d2AdTheta2 -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' d2AdThetadPhi -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' d2AdPhidTheta -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' Efunc -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' dEdTheta -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' d2EdTheta2 -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' d2EdThetadPhi -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...    
-%     ' d2EdPhidTheta -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ... 
-%     ' dEdPhi -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' d2EdPhi2 -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...    ' MI -args {zeros(1,NVAR),0,zeros(1,NPAR),0}' ...     
-%     ' S0 -args {zeros(1,NPAR)}' ...
-%     ' S20 -args {zeros(1,NPAR)}' ...
-%     ' SV0 -args {zeros(1,NPAR)}' ...
-%     ' S2V0 -args {zeros(1,NPAR)}' ...      
-%     ' Y0 -args {zeros(1,NPAR)}' ...    
-%     ' V0 -args {zeros(1,NPAR)}' ... 
-%     ' systemJacobian_diag -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' systemJacobian -args {zeros(1,NVAR),0,zeros(1,NPAR)}' ...
-%     ' -I /usr/include/c++/4.2.1/ -I /usr/include'], ...
-%     '../C');
-% 
-% codegen_cmd = strrep(codegen_cmd, 'NVAR', int2str(length(phi)));
-% codegen_cmd = strrep(codegen_cmd, 'NPAR', int2str(length(Theta)));
-% 
-% eval(codegen_cmd)
-% 
-% NVAR = int2str(length(phi));
-% NPAR = int2str(length(Theta));
-% 
-% objs1 = {'reactionFlux', 'J', 'dFdTheta', 'd2fdTheta2', 'Afunc', 'dAdTheta', ...
-%     'dAdPhi', 'd2AdPhi2', 'd2AdTheta2', 'd2AdThetadPhi', 'd2AdPhidTheta', ...
-%     'd2AdPhidTheta', 'Efunc', 'dEdTheta', 'd2EdTheta2', 'd2EdThetadPhi', ...
-%     'd2EdPhidTheta', 'dEdPhi', 'd2EdPhi2', 'systemJacobian', 'systemJacobian_diag'};
-% 
-% for o=objs1
-%     fprintf('%s ', o{:})
-%     tic
-%     eval(sprintf('codegen -c -d ../C %s -args {zeros(1,%s),0,zeros(1,%s)}', o{:}, NVAR, NPAR))
-%     t=toc;
-%     fprintf('%0.3f\n', t);
-% end
-% 
-% objs2 = {'S0','S20','SV0','S2V0','Y0','V0'};
-% for o = objs2
-%     fprintf('%s ', o{:});
-%     tic
-%     eval(sprintf('codegen -c -d ../C %s -args {zeros(1,%s)}', o{:}, NPAR))
-%     t=toc;
-%     fprintf('%0.3f\n', t);
-% end
-
-%% generate C code
 
 % requires phi, t, Theta
 objs1 = {'reactionFlux', 'J', 'dFdTheta', 'd2fdTheta2', 'Afunc', 'dAdTheta', ...
-    'dAdPhi', 'd2AdPhi2', 'd2AdTheta2', 'd2AdThetadPhi', 'd2AdPhidTheta', ...
-    'd2AdPhidTheta', 'Efunc', 'dEdTheta', 'd2EdTheta2', 'd2EdThetadPhi', ...
-    'd2EdPhidTheta', 'dEdPhi', 'd2EdPhi2',  'systemJacobian_diag'}; % 'systemJacobian',
+         'dAdPhi', 'd2AdPhi2', 'd2AdTheta2', 'd2AdThetadPhi', 'd2AdPhidTheta', ...
+         'd2AdPhidTheta', 'Efunc', 'dEdTheta', 'd2EdTheta2', 'd2EdThetadPhi', ...
+         'd2EdPhidTheta', 'dEdPhi', 'd2EdPhi2',  'systemJacobian_diag'};
 
 % requires only Theta
 objs2 = {'S0','S20','SV0','S2V0','Y0','V0'};
@@ -329,7 +219,6 @@ function Y0=solveSS_mre(S,F,phi) %#codegen
 % solve for the steady state with the parameters specified
 
 F = reshape(F,[],1);
-% tmp = solve(S*F,phi);
 tmp1=num2cell(S*F);
 tmp2=num2cell(phi);
 tmp = solve(tmp1{:},tmp2{:}); % solve for phi
@@ -438,7 +327,10 @@ end
 end
 
 function J=Jacobian(x,y)
+    J = jacobian(reshape(x,[],1),y);
+end
 
-% J = jacobian(reshape(x,1,[]),y);
-J = jacobian(reshape(x,[],1),y);
+function expr=simplifyExpr(expr)
+    [N,D] = numden(expr);
+    expr = N./expand(D);
 end
